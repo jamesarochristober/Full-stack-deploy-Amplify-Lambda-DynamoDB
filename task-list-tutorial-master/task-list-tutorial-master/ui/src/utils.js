87abfc6f328c57b738bf9a975d2c1564442b1483
josephajar <josephraja177@gmail.com>
2025-11-14 14:45:07 +0530

    feat: updated api url

--- task-list-tutorial-master/task-list-tutorial-master/ui/src/utils.js
+++ task-list-tutorial-master/task-list-tutorial-master/ui/src/utils.js
@@ -1,1 +1,2 @@
-export const API_URL = "https://4askztxm4bmyrtskmek74xqboi0ywlgq.lambda-url.us-east-1.on.aws/task";
+export const API_URL = 
+    "https://4askztxm4bmyrtskmek74xqboi0ywlgq.lambda-url.us-east-1.on.aws/task";
